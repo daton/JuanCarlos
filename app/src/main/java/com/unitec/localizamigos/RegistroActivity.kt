bd5f6667e487b7be1d65eebfd76b63e38ddda702
daton <rapidclimate@gmail.com>
2021-04-06 08:09:23 -0500

    sssssssssss

--- app/src/main/java/com/unitec/localizamigos/RegistroActivity.kt
+++ app/src/main/java/com/unitec/localizamigos/RegistroActivity.kt
@@ -12,6 +12,7 @@ import com.android.volley.Response
 import com.android.volley.toolbox.ImageLoader
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+
 import com.unitec.localizamigos.modelo.Usuario
 import org.json.JSONObject
 
